ebfff1f8e899ed3085185cce3ebec5aeb5b8b05d
Chethana Kalpa <chethanakh1998.09@gmail.com>
2024-04-19 14:32:20 +0530

    do vehicle crud

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+require('dotenv').config()
 const app = express();
 
 app.set('view engine', 'pug');
@@ -7,65 +8,83 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_PRIVATE_APP_KEY;
+const BASE_URI = "https://api.hubspot.com/crm/v3";
+const ObjectType = "2-26350164";
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+};
 
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
+app.get("/", async (req, res) => {
     try {
-        const resp = await axios.get(contacts, { headers });
+        const resp = await axios.get(`${BASE_URI}/objects/${ObjectType}?properties=regstration_number,type,features`, { headers });
         const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
+        res.render("index", { title: "Add Vehicle", data });
+    } catch (e) {
+        res.render("error", { code: 404, message: "something went wrong" });
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
+app.get("/add-cobj", async (req, res) => {
+    try {
+        res.render("add");
+    } catch (e) {
+        res.render("error", { code: 404, message: "something went wrong" });
+    }
+});
+
+app.post("/add-cobj", async (req, res) => {
+    try {
+        features = req.body.features
+        if (Array.isArray(features)) {
+            features = features.join(';')
         }
+        const addObj = {
+            properties: {
+                regstration_number: req.body.regstration_number,
+                type: req.body.type,
+                features: features ?? '',
+            },
+        };
+
+        var resp = await axios.post(`${BASE_URI}/objects/${ObjectType}`, addObj, { headers });
+        res.redirect("/update-cobj")
+    } catch (e) {
+        res.render("error", { code: 500, message: e?.response?.data?.message ?? e.message });
     }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
 });
-*/
 
+app.get("/update-cobj/:id", async (req, res) => {
+    try {
+        const resp = await axios.get(`${BASE_URI}/objects/${ObjectType}/${req.params.id}?properties=regstration_number,type,features`, { headers });
+        const data = resp.data.properties;
+        data['features'] = data['features'] ? data['features'].split(';') : []
+        res.render("update", { data });
+    } catch (e) {
+        res.render("error", { code: 500, message: e?.response?.data?.message ?? e.message });
+    }
+});
 
-// * Localhost
+app.post("/update-cobj/:id", async (req, res) => {
+    try {
+        features = req.body.features
+        if (Array.isArray(features)) {
+            features = features.join(';')
+        }
+        const updateObj = {
+            properties: {
+                regstration_number: req.body.regstration_number,
+                type: req.body.type,
+                features: features ?? '',
+            },
+        };
+        await axios.patch(`${BASE_URI}/objects/${ObjectType}/${req.params.id}`, updateObj, { headers });
+        res.redirect(`/update-cobj/${req.params.id}`,)
+    } catch (e) {
+        res.render("error", { code: 500, message: e?.response?.data?.message ?? e.message });
+    }
+});
+
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
